80aee72b5ddb9a444872fd4a5d02600c04d55004
Marcus Grant <marcusfg@gmail.com>
2018-05-22 21:26:51 -0400

    [bash] change to nvm to hopefully improve func of node dev

--- bash_exports.sh
+++ bash_exports.sh
@@ -102,6 +102,9 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --hidden --exclude .git --exclude node_modules --exclude .cache --exclude .local -t d . /"
 
 
+# n version manager, using nvm for now
+# export N_PREFIX=$HOME/.n
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
