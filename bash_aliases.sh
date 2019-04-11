fb5cee99f6c8924fd0fd0e58b57c4f055a461266
Marcus Grant <marcusg@fb.com>
2019-04-11 11:01:54 -0700

    fix glog alias to create a better branched git log

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -96,8 +96,7 @@ alias gca="git commit -v -a"
 alias gb="git branch"
 alias gba="git branch -a"
 alias gcam="git commit -am"
-alias glog="git log --format='%Cgreen%h%Creset %C(cyan)%an%Creset - %s' --graph"
-
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 
 
