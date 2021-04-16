a98ca99473004385bef36211633a77a9c34525f6
Marcus Grant <marcusfg@gmail.com>
2021-04-16 23:30:19 +0200

    Refact: bashrc now rc.bash, *_profile profile.bash

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,8 @@ RUN export EDITOR=nano
 RUN mkdir /root/.dots
 RUN mkdir /root/.dots/bash
 RUN rm /root/.bashrc
-RUN ln -sf /root/.dots/bash/bashrc.bash /root/.bashrc
-RUN ln -sf /root/.dots/bash/bashprofile.bash /root/.bash_profile
+RUN ln -sf /root/.dots/bash/rc.bash /root/.bashrc
+RUN ln -sf /root/.dots/bash/profile.bash /root/.bash_profile
 RUN git clone --depth=1 https://github.com/Bash-it/bash-it.git /root/.dots/bash/bash-it
 
 WORKDIR /root
---
+++ profile.bash
@@ -0,0 +1,41 @@
+# Get dotfile directory as DOTS_DIR & bash dotfiles subdir as DOTS_DIR_BASH
+_DOTSDIRSRC="${BASH_SOURCE[0]}"
+while [ -h "$_DOTSDIRSRC" ]; do # resolve $_DOTSDIRSRC until the file is no longer a symlink
+  DOTS_DIR_BASH="$( cd -P "$( dirname "$_DOTSDIRSRC" )" >/dev/null 2>&1 && pwd )"
+  _DOTSDIRSRC="$(readlink "$_DOTSDIRSRC")"
+  [[ $_DOTSDIRSRC != /* ]] && _DOTSDIRSRC="$DOTS_DIR_BASH/$_DOTSDIRSRC" # if $_DOTSDIRSRC was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DOTS_DIR_BASH="$( cd -P "$( dirname "$_DOTSDIRSRC" )" >/dev/null 2>&1 && pwd )"
+DOTS_DIR="$(dirname "$DOTS_DIR_BASH")"
+
+# Load custom local-only overrides early in case there's stuff here to override
+if [ -f $DOTS_DIR_BASH/bashlocal.bash ]; then source $DOTS_DIR_BASH/bashlocal.bash; fi
+
+# Set BASH_IT directory and clone it if it doesn't exist already
+if [ -z $BASH_IT ]; then export BASH_IT="$DOTS_DIR_BASH/bash-it"; fi
+if [ ! -d $BASH_IT ]; then
+    echo
+    echo "Bash-It not installed, installing now..."
+    echo
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git $BASH_IT
+fi
+
+# Setup Basher if it doesn't already exist
+# BASHER_ROOT is where basher will look for all its lookups
+# BASHER_PREFIX is where it will install all packages
+if [ -z $BASHER_ROOT ]; then export BASHER_ROOT="$DOTS_DIR_BASH/basher"; fi
+if [ -z $BASHER_PREFIX ]; then export BASHER_PREFIX="$BASHER_ROOT/cellar"; fi
+if [ ! -d $BASHER_ROOT ]; then
+    echo
+    echo "Basher not installed, installing now..."
+    echo
+    git clone --depth=1 https://github.com/basherpm/basher.git $BASHER_ROOT
+fi
+
+# Should this be in profile? https://unix.stackexchange.com/a/26059
+export PATH="$PATH:$BASHER_ROOT/bin"
+eval "$(basher init - bash)" # replace `bash` with `zsh` if you use zsh
+
+# Some defaults
+export EDITOR="vim"
+
