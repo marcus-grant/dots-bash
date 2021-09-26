0f1cf60475c0043184c5bc2772d2ae0e8d16064e
Marcus Grant <marcusfg@gmail.com>
2021-09-26 22:14:05 +0200

    Refct: Remove all commented out v2 stuff

--- profile.bash
+++ profile.bash
@@ -14,34 +14,6 @@ DOTS_DIR="$(dirname "$DOTS_DIR_BASH")"
 # Load custom local-only overrides early in case there's stuff here to override
 if [ -f $DOTS_DIR_BASH/bashlocal.bash ]; then source $DOTS_DIR_BASH/bashlocal.bash; fi
 
-# DELETEME: After change over away from Bash-It is complete
-# Set BASH_IT directory and clone it if it doesn't exist already
-# if [ -z $BASH_IT ]; then export BASH_IT="$DOTS_DIR_BASH/bash-it"; fi
-# if [ ! -d $BASH_IT ]; then
-#     echo
-#     echo "Bash-It not installed, installing now..."
-#     echo
-#     git clone --depth=1 https://github.com/Bash-it/bash-it.git $BASH_IT
-# fi
-
-# DELETEME: After v3 works
-# Setup Basher if it doesn't already exist
-# BASHER_ROOT is where basher will look for all its lookups
-# BASHER_PREFIX is where it will install all packages
-# if [ -z $BASHER_ROOT ]; then export BASHER_ROOT="$DOTS_DIR_BASH/basher"; fi
-# if [ -z $BASHER_PREFIX ]; then export BASHER_PREFIX="$BASHER_ROOT/cellar"; fi
-# if [ ! -d $BASHER_ROOT ]; then
-#     echo
-#     echo "Basher not installed, installing now..."
-#     echo
-#     git clone --depth=1 https://github.com/basherpm/basher.git $BASHER_ROOT
-# fi
-
-# DELETEME: Part of basher
-# Should this be in profile? https://unix.stackexchange.com/a/26059
-# export PATH="$PATH:$BASHER_ROOT/bin"
-# eval "$(basher init - bash)" # replace `bash` with `zsh` if you use zsh
-
 # Some defaults
 export EDITOR="vim"
 source $DOTS_DIR_BASH/rc.bash
