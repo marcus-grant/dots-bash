22e936236b31010af903708b7c1fa07c36b1bc40
Marcus Grant <marcusfg@protonmail.com>
2021-11-13 19:09:53 +0100

    Env: Add man page link to pass.bash

--- plugins/pass.bash
+++ plugins/pass.bash
@@ -1,3 +1,4 @@
 # Bash configuration plugin for pass password-store
+# Manual page here: https://git.zx2c4.com/password-store/about
 PASSWORD_STORE_DIR="$HOME/.config/nycklar/pass"
 PASSWORD_STORE_CLIP_TIME=300
